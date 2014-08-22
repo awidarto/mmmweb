d129ff3f4af786ef19a2cc9762fc01e40993e0a1
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-23 03:33:02 +0700

    change theme

--- public/themes/default/views/layout/css.blade.php
+++ public/themes/default/views/layout/css.blade.php
@@ -2,8 +2,8 @@
 
     {{ HTML::style('css/jquery-fileupload/css/jquery.fileupload-ui.css') }}
 
-    {{ HTML::style('css/app2.css') }}
+    {{-- HTML::style('css/app2.css') }}
 
     {{-- HTML::style('css/form.css') --}}
 
-    {{ HTML::style('css/gridtable.css') }}
+    {{-- HTML::style('css/gridtable.css') --}}
