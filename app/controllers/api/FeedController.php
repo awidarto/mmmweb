5001b9a582fc63c41359ae7759cd7af1ac0be7eb
Andy Awidarto <andy.awidarto@gmail.com>
2014-12-15 22:50:21 +0700

    api route

--- app/controllers/api/FeedController.php
+++ app/controllers/api/FeedController.php
@@ -13,71 +13,15 @@ class FeedController extends \BaseController {
 	public function index()
 	{
 		//
+        $lastrefresh = Input::get('lastrefresh');
 
-        $media = \Media::where('status','approved')->orderBy('createdDate','desc')->get();
+        $mlast = new MongoDate($lastrefresh);
 
-        for( $i = 0; $i < count($media); $i++ ){
+        $media = \Feed::where( 'timestamp', '>', $mlast )
+            ->orderBy('timestamp','desc')
+            ->get();
 
-                $media[$i]->mongoid = $media[$i]->_id;
-
-                $media[$i]->token = $media[$i]->_token;
-
-                unset($media[$i]->_id);
-                unset($media[$i]->_token);
-
-                unset($media[$i]->thumbnail_url);
-                unset($media[$i]->large_url);
-                unset($media[$i]->medium_url);
-                unset($media[$i]->full_url);
-                unset($media[$i]->delete_type);
-                unset($media[$i]->delete_url);
-                unset($media[$i]->filename);
-                unset($media[$i]->filesize);
-                unset($media[$i]->temp_dir);
-                unset($media[$i]->filetype);
-                unset($media[$i]->is_image);
-                unset($media[$i]->is_audio);
-                unset($media[$i]->is_video);
-                unset($media[$i]->fileurl);
-                unset($media[$i]->file_id);
-                unset($media[$i]->caption);
-
-                // non file related
-                unset($media[$i]->lyric);
-                unset($media[$i]->files);
-
-                $dm = $media[$i]->defaultmedias;
-
-                unset($dm['delete_type']);
-                unset($dm['delete_url']);
-                unset($dm['temp_dir']);
-
-                $media[$i]->defaultmedias = $dm;
-
-                foreach($dm as $k=>$v){
-                    $name = 'media'.str_replace(' ', '', ucwords( str_replace('_', ' ', $k) ));
-                    $media[$i]->{$name} = $v;
-                }
-                unset($media[$i]->defaultmedias);
-
-                $dp = $media[$i]->defaultpictures;
-
-                unset($dp['delete_type']);
-                unset($dp['delete_url']);
-                unset($dp['temp_dir']);
-
-                foreach($dp as $k=>$v){
-                    $name = 'picture'.str_replace(' ', '', ucwords( str_replace('_', ' ', $k) ));
-                    $media[$i]->{$name} = $v;
-                }
-                unset($media[$i]->defaultpictures);
-
-                $media[$i]->createdDate = date('Y-m-d H:i:s',$media[$i]->createdDate->sec);
-                $media[$i]->lastUpdate = date('Y-m-d H:i:s',$media[$i]->lastUpdate->sec);
-
-        }
-
-        return $media;
+        return Response::json($media->toArray());
 	}
 
 
